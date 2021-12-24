da4b021f155a679ace67ec4cf7ebe1d6653b65d7
Vincent Nguyen <72878425+vincentbanguyen@users.noreply.github.com>
2021-12-23 23:35:23 -0600

    Add files via upload

---
+++ graphPractice.py
@@ -0,0 +1,20 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+# ADD NODES
+#G.add_node('A')
+
+# ADD EDGES
+#G.add_edge('A','B')
+
+G = nx.complete_graph(10)
+
+
+print(nx.info(G))
+
+
+# SHOW GRAPH
+nx.draw(G, with_labels = True)
+plt.show()
