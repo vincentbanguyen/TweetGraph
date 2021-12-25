393c010ca820929bf5734cc3f4178c4936b0f397
Tommytran30 <96552388+Tommytran30@users.noreply.github.com>
2021-12-24 19:18:39 -0600

    get a list of dictionaries from the printed data
    Created a function get_username to obtain the data and return a list of dictionaries of each users userid and username

--- TweetGraph.py
+++ TweetGraph.py
@@ -43,7 +43,22 @@ def main():
     url = create_url()
     params = get_params()
     json_response = connect_to_endpoint(url, params)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    data_print = json.dumps(json_response, indent=4, sort_keys=True)
+
+    print(data_print)
+    return data_print
+    
+def get_usernames(data_print):
+    data_list = json.loads(data_print)
+    #print(data_list['data'][0]['username']) 
+        #^ this was to test which part of the dictionary and list i needed to iterate through 
+    username_list = []
+    for i in range(len(data_list['data'])):
+        username = data_list['data'][i]['username']
+        user_ID = data_list['data'][i]['id']
+        username_list.append({user_ID:username})
+    print(username_list)
+    return(username_list)
 
 
 if __name__ == "__main__":
