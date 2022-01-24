488aa4d03ca3bb1c0f637fe3e86909d0f5b3db83
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2022-01-24 02:45:34 -0800

    Check if user in a gen likes the same tweet

--- TweetGraph.py
+++ TweetGraph.py
@@ -14,6 +14,10 @@ class User(object):
         self.userID = userID
         self.userimgURL = userimgURL
 
+class Tweet(object):
+    def __init__(self, id, text):
+        self.id = id
+        self.text = text
 
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='AAAAAAAAAAAAAAAAAAAAAG1IXQEAAAAAUd1GAqrl4tKWAUyqcUWJ7oB5hE8%3DwxHZaWNPhe8JaQew1nDLRgqUHdA14GKbA9Z0OWoWRtt5rEnpOj'
@@ -28,7 +32,8 @@ auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-counter = 0 #counts the number of follow pulls (should not exceed 15 per 15 min)
+following_counter = 0 #counts the number of following lists pulls (should not exceed 15 per 15 min)
+likes_counter = 0 #counts the number of like lists pulls (should not exceed 75 per 15 min)
 
 # CONNECTING TO TWITTER API
 def bearer_oauth(r):
@@ -73,8 +78,8 @@ def get_connected_users(start_user):
         print("User has no connections")
         return []
 
-def get_3following_users(start_user): # Get First 3 Following of user
-    following_users = get_following_users(start_user)
+def get_3following_users(user: User): # Get First 3 Following of user
+    following_users = get_following_users(user)
     first3_following = []
     count = 0
     for following_user in following_users:
@@ -85,6 +90,38 @@ def get_3following_users(start_user): # Get First 3 Following of user
             return first3_following
     print("User is only following " + str(count) + " accounts")
     return first3_following
+
+def get_liked_tweets(user: User):
+    global likes_counter
+    likes_counter+=1
+    print("# of likes list pulled: " + str(likes_counter))
+    url = "https://api.twitter.com/2/users/{}/liked_tweets".format(user.userID)
+    params = {"user.fields": "profile_image_url"}
+    json_response = connect_to_endpoint(url, params)
+    following_data = json.dumps(json_response, indent=4, sort_keys=True)
+    data_list = json.loads(following_data)
+    liked_tweets_list = []
+    if len(data_list) > 1:
+        for i in range(len(data_list['data'])):
+            id = data_list['data'][i]['id']
+            text = data_list['data'][i]['text']
+            liked_tweet = Tweet(id, text)
+            liked_tweets_list.append(liked_tweet)
+    return liked_tweets_list
+
+def check_liked_same_tweet(likes_of_start_user, user: User):
+   # likes_of_start_user = get_liked_tweets(start_user)
+    same_liked_tweets = []
+    count = 0
+    for like2 in get_liked_tweets(user):
+        count+=1
+        for like1 in likes_of_start_user:
+            if like2.id == like1.id:
+                print(user.username + " liked this same tweet: ")
+                print(like2.text)
+                same_liked_tweets.append(like2)
+    print("# tweets liked pulled: " + str(count))
+    return same_liked_tweets
 
 def get_profile_image(user_ID: int): # RETURNS IMAGE URL OF INPUTTED USER ID
     url = "https://api.twitter.com/2/users/{}".format(user_ID)
@@ -107,13 +144,13 @@ def get_following_list_profile_images(user: User):
 
 
 def get_following_users(user: User):
-    global counter
-    if counter == 15:
-        counter = 0
+    global following_counter
+    if following_counter == 15:
+        following_counter = 0
         print("Wait for 15 min")
         time.sleep(901)
-    counter+=1
-    print(counter)
+    following_counter+=1
+    print("Pulling following of user #" + str(following_counter))
     print("getting following list of: " + str(user.username))
     url = "https://api.twitter.com/2/users/{}/following".format(user.userID)
 
@@ -154,7 +191,7 @@ def test_exception_rate(start_user):
 # EXECUTE CODE HERE ---------------------------------------------------------------------------------
 def main():
     profile_images = []
-    username = "VincentBaNguyen"
+    username = "SpaceX"
     user_id = get_user_id(username)
     imgURL = get_profile_image(user_id)
     start_user = User(username, user_id, imgURL)
@@ -166,16 +203,22 @@ def main():
     G = nx.Graph()
     G.add_node(username, image = user_profile_img) #start user
 
+    likes_of_start_user = get_liked_tweets(start_user)
+
     following_users_1st = get_3following_users(start_user)
     add_nodes(G, following_users_1st, username, profile_images) #first gen
     for user in following_users_1st:
+        check_liked_same_tweet(likes_of_start_user, user)
         following_users_2nd = get_3following_users(user)
         add_nodes(G, following_users_2nd, user.username, profile_images) #second gen
         for user in following_users_2nd:
+            check_liked_same_tweet(likes_of_start_user, user)
             following_users_3rd = get_3following_users(user)
             add_nodes(G, following_users_3rd, user.username, profile_images) #third gen
-    
+            # for user in following_users_3rd:
+            #     check_liked_same_tweet(likes_of_start_user, user)
 
+    
     pos=nx.circular_layout(G)
 
     fig=plt.figure(figsize=(5,5))
